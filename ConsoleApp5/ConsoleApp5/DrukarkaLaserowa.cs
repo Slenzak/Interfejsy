41e6df15ffae88d352410043b993fc07f130a1d3
Slenzak <104827589+Slenzak@users.noreply.github.com>
2023-11-28 15:41:17 +0100

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/DrukarkaLaserowa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class DrukarkaLaserowa : IDrukarka
+    {
+        void IDrukarka.Drukuj()
+        {
+
+        }
+        public void Drukuj()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp5;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<IDzwiek> Idzwiek = new List<IDzwiek>();
+        Idzwiek.Add(new Samochod());
+        Idzwiek.Add(new Telefon());
+        foreach (IDzwiek idzwiek_ in Idzwiek)
+        {
+            idzwiek_.OdtwarzajDzwiek();
+        }
+
+    }
+    
+}
+public static class UzytkownikExtension
+    {
+        public static void Loguj(this Uzytkownik uzytkownik)
+        {
+
+        }
+    }
